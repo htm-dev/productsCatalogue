d831c19bf5409d23c00d76fefd442414b9c9b7b1
Haitam <riadihaitam@gmail.com>
2019-09-24 10:11:01 +0100

    products catalogue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProduitsComponent} from './produits/produits.component';
+import {AddProductComponent} from './add-product/add-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path : 'products' , component : ProduitsComponent
+},
+  {path : 'add-product' , component : AddProductComponent },
+  {path : '', redirectTo: '/products' , pathMatch : 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitsComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
