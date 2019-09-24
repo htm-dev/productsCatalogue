881d7ef15b24d17ecc69efa8f8a644828ccb6c64
Haitam <riadihaitam@gmail.com>
2019-09-24 18:37:24 +0100

    V1 Terminer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProduitsComponent} from './produits/produits.component';
 import {AddProductComponent} from './add-product/add-product.component';
+import {EditProductComponent} from './edit-product/edit-product.component';
 
 const routes: Routes = [{
   path : 'products' , component : ProduitsComponent
 },
   {path : 'add-product' , component : AddProductComponent },
-  {path : '', redirectTo: '/products' , pathMatch : 'full'}];
+  {path : '', redirectTo: '/products' , pathMatch : 'full'},
+  {path : 'edit-product/:id' , component : EditProductComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { ProduitsComponent } from './produits/produits.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { EditProductComponent } from './edit-product/edit-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProduitsComponent,
-    AddProductComponent
+    AddProductComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
